5d2e3a59be8a3e82f69a55e384e627f42cbda546
Ryan Brantley <ryan.brantley@onemednet.com>
2022-05-21 11:38:31 -0400

    #7 regex arg to pull content

--- py_docx_rip.py
+++ py_docx_rip.py
@@ -1,11 +1,30 @@
 #modules used for extracting information from word / xml documents
-import os, zipfile, re, subprocess
+import os
+import re
+import subprocess
 import xml.dom.minidom
+import zipfile
 
 #Set root for ripper
-ripbase = "/Users/ryanbrantley/Library/CloudStorage/OneDrive-SharedLibraries-OneMedNet/Compliance - Word Docs"
+ripbase = "/Users/ryanbrantley/Library/CloudStorage/OneDrive-SharedLibraries-OneMedNet/Compliance - Word Docs/"
 
 print(ripbase)
 
-bash_cmd = "tree "+ripbase
-process = subprocess.run(bash_cmd, shell=True, check=True, executable='/bin/bash')
+
+### test tree bash run for loops later
+    #bash_cmd = "tree "+ripbase
+    #process = subprocess.run(bash_cmd, shell=True, check=True, executable='/bin/bash')
+
+## POC for Word docx drill down into XML content
+doc = ripbase+"0XX Corporate WIP/SOP-015 Business Continuity/SOP-015 Business Continuity.docx"
+
+docz = zipfile.ZipFile(doc, mode="r")
+docz.namelist()
+
+ripxml = xml.dom.minidom.parseString(docz.read('word/document.xml')).toprettyxml(indent='  ')
+ripxml = str(docz.read('word/document.xml'))
+docz.close
+
+#quick and dirty regex pattern match to grab raw content found between <w:t> tags
+raw = re.findall('<w:t(?:>| xml:space="preserve">)(.*?)<.+?>', ripxml)
+print(raw)
