b5ea3ee1424f68a8b27a11af07182f43440a6d36
Ryan Brantley <ryan.brantley@onemednet.com>
2022-06-03 11:06:49 -0400

    updated format regex logic

--- py_docx_rip.py
+++ py_docx_rip.py
@@ -3,6 +3,7 @@ import os
 import re
 import xml.dom.minidom
 import zipfile
+from atlassian import confluence
 
 # Set root for ripper
 riproot = "/Users/ryanbrantley/Library/CloudStorage/OneDrive-SharedLibraries-OneMedNet/Compliance - Word Docs/"
@@ -19,13 +20,12 @@ def docx_file(file):
 
 # function to get the path to the file to define breadcrumb taxonomy
 def doc_tax(path):
+    slices = []
     cookie = path.split("/")
     if (len(cookie) >= 1 and range(len(cookie))):
-        slices = []
         for c in range(len(cookie)):
-            crumb = cookie[c]
-            slices.append(crumb)
-        return(slices)
+            slices.append(cookie[c])
+    return(slices)
 
 # pipe the XML content section of docx to string
 def str_xml(file):
@@ -36,39 +36,64 @@ def str_xml(file):
     return ripxml
     
 # pull the relevant groups and sections out of the docx XML
-def docx_content(docx):
-    docx_sect = []
-    praw = re.findall(r"<w:pStyle w:val=\"([\S]+)\"\/>[.\s\S]*?(<w:(?:t|t xml:[.\S]+)>(?:[.\s\S]+?)<\/w:p>)", docx)
-    for row in praw:
-        tag = row[0]
-        for val in row:
-            nest = re.findall(r"(?:<w:(?:t|t xml:[.\S]+)>([.\s\S]+?)<\/w:t>)",val)
-            s = ' '.join(nest)
-        docx_sect.append([tag, s])
-    return docx_sect
+def nsx(doc):
+        out = []
+        scan = re.findall(r'(<w[^>]+(?:/>|>)(?:.*?)</w:t>)',doc)
+        style_scan = re.compile(r'<w:pStyle w:val=\"([\S]+)\"')
+        content_scan = re.compile(r'(?:<w:t(?: xml.*?\">|>))(.*?)</w:t>')
+
+        for w in range(len(scan)):
+            if style_scan.findall(scan[w]):    
+                dx_style = style_scan.findall(scan[w])[0]
+            else:
+                dx_style = "default"
+
+            if content_scan.findall(scan[w]):
+                content = content_scan.findall(scan[w])[0]
+                cdict = {
+                    "style": dx_style,
+                    "body": content
+                }
+                out.append(cdict)
+                
+        return out
 
 def tree_printer(riproot):
     payload = []
-    for root, dirs, files in os.walk(riproot):
+
+    for root, dirs, files in os.walk(riproot): #walk the dir specified for docs
         for f in range(len(files)):
             file = files[f]
-            if not dotfile_file(file): ### skip dotfiles
-                taxroot = root.split(riproot)[1] #capture raw taxonomy root of SOP
-                if docx_file(file): ### what do we do to docx files??
+
+            if not dotfile_file(file): #skip dotfiles
+                taxroot = root.split(riproot)[1] #capture taxonomy root of SOP
+                
+                if docx_file(file):
                     content = str_xml(root+"/"+file)
-                    content = docx_content(content)
+                    content = nsx(content)
                     rip_out = {
                         'file': file,
+                        'root': root,
                         'taxonomy': taxroot,
                         'content': content
                     }
                     payload.append(rip_out)
-                    print("BING! BONG!")
-                if not docx_file(file):
-                    print("DO THE OTHER FILE HANDLER FOR:"+file)
-                    #do pdf and vsd attachment
-    
+                
+                if not docx_file(file): #handle non-docx files that are not dotfiles
+                    pass
+
     return payload
 
-files = tree_printer(riproot)
-print(files)
+def uq(alist):
+    out = []
+    alist = (list(alist))
+    for x in range(len(alist)):
+       for y in range(len(alist[x]["content"])):
+            if alist[x]["content"][y]["style"] not in out:
+                out.append(str(alist[x]["content"][y]["style"]))
+    return out
+
+
+allofit = tree_printer(riproot)
+
+print(*uq(allofit), sep="\n")
